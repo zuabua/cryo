5216ecc31c7788e226978ace3b9b7a917f81b3b9
zuabua <emil@erasmussen.dk>
2026-07-19 13:43:18 +0200

    colors added

---
+++ theme/.config/theme/colors.sh
@@ -0,0 +1,30 @@
+# Cryo color pallette
+
+# Typography
+export CRYO_FONT="JetBrainsMono Nerd Font"
+
+# Base Layers
+export CRYO_BASE="#303446"
+export CRYO_SURFACE="#292c3c"
+export CRYO_OVERLAY="#414559"
+
+# Accents
+export CRYO_ACCENT_TEAL="#5eead4"
+export CRYO_ACCENT_INDIGO="#818cf8"
+
+# Text
+export CRYO_TEXT="#c6d0f5"
+export CRYO_SUBTEXT="#a5adce"
+
+# Semantic
+export CRYO_RED="#f87171"
+
+# Convenience without the hexes
+export CRYO_BASE_HEX="303446"
+export CRYO_SURFACE_HEX="292c3c"
+export CRYO_OVERLAY_HEX="414559"
+export CRYO_ACCENT_TEAL_HEX="5eead4"
+export CRYO_ACCENT_INDIGO_HEX="818cf8"
+export CRYO_TEXT_HEX="c6d0f5"
+export CRYO_SUBTEXT_HEX="a5adce"
+export CRYO_RED_HEX="f87171"
